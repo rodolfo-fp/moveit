6941ad6e0d2e19b23a4e892633553e9c9737dd30
Rodolfo Francisco Pinto <rodolfofrancisco1990@gmail.com>
2021-03-28 12:10:46 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://github.com/rodolfo-fp.png"
+        alt="Rodolfo Francisco Pinto"
+      />
+      <div>
+        <strong>Rodolfo Francisco Pinto</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
